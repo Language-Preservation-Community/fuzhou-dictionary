71add3b97b174a872c94dc76a49ad46989068209
programmeruser <69394951+programmeruser2@users.noreply.github.com>
2021-07-26 22:57:29 +0000

    git pull messed up my code smh, hopefully I fixed it

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+const config = require('./config');
 const fs = require('fs');
 const search = require('./search');
-let raw = fs.readFileSync('data.json');
+
+let raw = fs.readFileSync(config.database);
 search.init(raw);
 let data = search.getData();
 
@@ -9,6 +11,7 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 app.use('/static', express.static('static'));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
 	res.render('index');
@@ -34,7 +37,7 @@ app.get('/search', (req, res, next) => {
 	const searchResults = search.search(type, query);
 	res.render('search', { searchResults });
 });
-app.get('/entry/:entry', (req, res) => {
+app.get('/entry/:entry', (req, res, next) => {
 	const entryId = Number(req.params.entry);
 	if (!Number.isInteger(entryId)) {
 		const err = new Error('Invalid entry ID#');
@@ -53,6 +56,40 @@ app.get('/entry/:entry', (req, res) => {
 	entry._index = entryId;
 	res.render('entry', entry);
 });
+
+if (config.suggestions) {
+	const { verify: hcaptchaVerify } = require('hcaptcha');
+	const { Octokit } = require('@octokit/rest');
+	const octokit = new Octokit({ auth: config.githubToken });
+	app.get('/suggest', (req, res) => {
+		res.render('suggest', { hcaptchaSitekey: config.hcaptchaSitekey });
+	});
+	app.post('/suggest', async (req, res, next) => {
+		const { githubUsername, word, info } = req.body;
+		const hcaptchaToken = req.body['h-captcha-response'];
+		if (!githubUsername || !word || !info || !hcaptchaToken) {
+			const err = new Error('One or more form parameters empty or nonexistent. Please enter "ghost" as your Github username if you do not wish to share your Github account');
+			err.status = 400;
+			return next(err);
+		}
+		const { success: hcaptchaSuccess } = await hcaptchaVerify(config.hcaptchaSecret, hcaptchaToken);
+		if (!hcaptchaSuccess) {
+			const err = new Error('hCaptcha token verification failed. Please try again.');
+			err.status = 400;
+			return next(err);
+		}
+		const comment = `User: @${githubUsername}
+Word: ${word}
+Info: ${info}`;
+		const { data: { html_url: issueCommentUrl } } = await octokit.rest.issues.createComment({
+			owner: config.suggestionsIssue.owner,
+			repo: config.suggestionsIssue.repo,
+			issue_number: config.suggestionsIssue.issue,
+			body: comment
+		});
+		res.render('suggest_success', { issueCommentUrl });
+	});
+}
 
 process.on('SIGUSR2', () => {
 	console.log('Received SIGUSR2, reloading database...');
